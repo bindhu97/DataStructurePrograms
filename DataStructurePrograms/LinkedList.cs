95931ee3e46f66cd6215ae7f5184a99e82c8d990
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-01 20:35:26 +0530

    [Bindhu]Add: Unordered List

---
+++ DataStructurePrograms/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    public class LinkedList<T>
+    {
+        public Node<T> head;
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0}=> Word add in list", node.data);
+        }
+        public void Search(T value)
+        {
+            Node<T> temp = this.head;
+            while (temp != null)
+            {
+                if (temp.data.Equals(value))
+                {
+                    Console.WriteLine("This Word {0} is found ", temp.data);
+                    return;
+                }
+                temp = temp.next;
+            }
+            if (temp == null)
+            {
+                Console.WriteLine("{0} This Word is not found", value);
+                Add(value);
+            }
+        }
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkeList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructurePrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿using DataStructurePrograms;
+using System;
+using System.Collections.Generic;
+
+namespace Data_Structure_Problems
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            string text = "Search Happiness in everything";
+            string[] list = text.Split(" ");
+            int count = 0;
+            while (count < list.Length)
+            {
+                linkedList.Add(list[count++]);
+            }
+            linkedList.Display();
+            Console.WriteLine("\n");
+            linkedList.Search("Happiness");
+        }
+    }
+}
